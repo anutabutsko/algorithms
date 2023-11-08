49ca5123c0fa47d535be162dce44d198d227e30f
anutabutsko <anutabutsko@gmail.com>
2023-11-07 20:16:33 -0500

    continue

--- src/depth_first_search/depth_first_search.py
+++ src/depth_first_search/depth_first_search.py
@@ -1,8 +1,12 @@
+import sys
 from time import perf_counter
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 from graph import generate_graph
+
+sys.setrecursionlimit(10000)  # Set a higher recursion limit
 
 
 # Function to convert an adjacency matrix to a dictionary representation of the graph.
@@ -25,10 +29,11 @@ def convert(graph):
 
 # Function to perform Depth First Search on the graph.
 def DFS(graph):
-    # Convert the graph to a dictionary representation.
-    graph = convert(graph)
     # Initialize time to track the discovery and finish times.
     time = [0]
+
+    # Initialize a dictionary to track the greatest distance.
+    greatest_distance = {'value': 0}
 
     # Initialize all vertices in the graph as unvisited, marking them with color "white".
     for vertex in graph:
@@ -39,13 +44,14 @@ def DFS(graph):
     for vertex in graph:
         # If the vertex is unvisited, start the exploration process.
         if graph[vertex]["color"] == "white":
-            explore(graph, vertex, time)
+            current_distance = 0
+            explore(graph, vertex, time, current_distance, greatest_distance)
 
-    return graph
+    return graph, greatest_distance
 
 
 # Function to explore the graph starting from a given vertex.
-def explore(graph, vertex, time):
+def explore(graph, vertex, time, current_distance, greatest_distance):
     # Increment the discovery time for this vertex.
     time[0] += 1
     graph[vertex]["discovery_time"] = time[0]
@@ -58,7 +64,8 @@ def explore(graph, vertex, time):
         # If the connected vertex is unvisited, recursively explore it.
         if graph[connection]["color"] == "white":
             graph[connection]["previous"] = vertex
-            explore(graph, connection, time)
+            current_distance += 1  # Increment distance for this path.
+            explore(graph, connection, time, current_distance, greatest_distance)
 
     # Once exploration is complete, mark the vertex as finished.
     graph[vertex]["color"] = "black"
@@ -66,6 +73,9 @@ def explore(graph, vertex, time):
     # Increment the finish time for this vertex.
     time[0] += 1
     graph[vertex]["finish_time"] = time[0]
+
+    # Update the greatest distance if the current path is the longest.
+    greatest_distance['value'] = max(greatest_distance['value'], current_distance)
 
 
 # Check if the graph is connected.
@@ -91,15 +101,17 @@ def compute_search_time(runs, n=None):
     total_time = 0
 
     for _ in range(runs):
-        # Start timer.
-        start = perf_counter()
-
         # Generate graph without setting seed value to ensure a new graph is generated every time.
         if n:
             # Adjust number N.
-            graph = generate_graph(n)
+            graph = generate_graph(n=n)
         else:
             graph = generate_graph()
+
+        # Convert the graph to a dictionary representation.
+        graph = convert(graph)
+        # Start timer.
+        start = perf_counter()
         # Run DFS on the generated graph.
         DFS(graph)
         # Stop timer.
@@ -112,21 +124,7 @@ def compute_search_time(runs, n=None):
     # Compute the average time by dividing the total time by the number of runs.
     average_time = total_time / runs
 
-    return f"The average time taken for DFS search over {runs} runs is {average_time:.6f} seconds."
-
-
-# Find the greatest distance between any two connected points in the graph
-def greatest_distance(graph):
-    greatest = 0
-
-    # Iterate over each vertex in the result.
-    for vertex in graph:
-        # Check if the finish time of the current vertex is greater than the current greatest distance.
-        if graph[vertex]["finish_time"] > greatest:
-            # If true, update the greatest distance to the finish time of this vertex.
-            greatest = graph[vertex]["finish_time"]
-
-    return f"The greatest distance between any two connected points in the graph is {greatest}."
+    return average_time
 
 
 # MAIN SCRIPT
@@ -144,26 +142,46 @@ if (graph == graph.transpose()).all():
 # QUESTION 2: Implement depth-first search. Is the underlying graph connected?
 # How long does it take the search to run on average? (be precise concerning what you are measuring).
 
+# Convert the graph to a dictionary representation.
+graph_dict = convert(graph)
+
 # Perform Depth First Search on the graph and print the result.
-result = DFS(graph)
+result, greatest_dist = DFS(graph_dict)
 print(result)
 
 # Check if the underlying graph is connected.
 print(check_if_connected(result))
 
 # Average search time over 10000 runs.
-print(compute_search_time(10000))
+print(f"The average time taken for DFS search over 10000 runs is {round(compute_search_time(10000), 6):.6f} seconds.")
 
 # QUESTION 3: What is the greatest distance between any two connected points in the graph?
-print(greatest_distance(result))
+print(f"The greatest distance between any two connected points in the graph is {greatest_dist['value']}.")
 
 # QUESTION 5: Change the random integer calls in lines 16, 20 and 24 to draw either 0 or 1. Does the graph remain connected?
 new_graph_1 = generate_graph(start=0, end=1)
-new_result_1 = DFS(new_graph_1)
+# Convert the graph to a dictionary representation.
+new_graph_dict = convert(new_graph_1)
+new_result_1, greatest_dist = DFS(new_graph_dict)
 print(check_if_connected(new_result_1))
 
 # QUESTION 6: Increase N in line 14. Does the runtime for DFS scale as expected? Provide a plot to back your conclusion.
-# Average search time over 10000 runs with N = 1000.
-print(compute_search_time(10000, n=1000))
+# Average search time over 1000 runs with N = 100.
+print(f"The average time taken for DFS search over 1000 runs for the graph size 100 is {round(compute_search_time(1000, n=100), 6):.6f} seconds.")
 
+# Plotting.
+sizes = [i for i in range(10, 1000, 10)]
+times = []
 
+for size in sizes:
+    times.append(compute_search_time(10, n=size))
+    print(size)
+
+plt.plot(sizes, times)
+plt.xlabel('Size of the graph.')
+plt.ylabel('Time')
+plt.title('Depth First Search Runtime')
+
+plt.grid(True)
+
+plt.show()
