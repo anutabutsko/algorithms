6e44828b6ff7c4819f4abbf18da4961c6f6ca6d2
anutabutsko <anutabutsko@gmail.com>
2023-12-12 21:34:15 -0500

    graph algorithms

---
+++ src/graph_algorithms/breadth_first_search/BFS.py
@@ -0,0 +1,9 @@
+"""
+A natural approach to starting such an investigation is to begin by
+first exploring all nodes directly connected to source by an edge.
+Having completed this task, consider studying all nodes connected
+by two edges to source and repeat.
+This ”shelling” can be formalized and goes by the name of breadth
+first search.
+"""
+
